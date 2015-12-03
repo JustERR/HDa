94a061d00f90364b97cbf3825fceeff83a222aa0
Ilya Bukhman <justerr@gmail.com>
2015-12-03 09:45:58 +0500

    Column added

--- Tables/dbo_Customers.sql
+++ Tables/dbo_Customers.sql
@@ -5,6 +5,7 @@
   [Address] [nvarchar](500) NULL,
   [City] [int] NULL,
   [IsLead] [int] NULL CONSTRAINT [DF_Customers_IsLead] DEFAULT (0),
+  [DateLastOrder] [datetime] NULL,
   CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([ID]),
   CONSTRAINT [FK_Customers_Customers] FOREIGN KEY ([ID]) REFERENCES [dbo].[Customers] ([ID])
 )
