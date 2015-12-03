09ac2695e92e6db70ef92ed58570d891e943fb8c
Ilya Bukhman <justerr@gmail.com>
2015-12-03 09:44:09 +0500

    Initial

---
+++ Tables/dbo_Customers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Customers] (
+  [ID] [int] IDENTITY,
+  [Name] [nvarchar](150) NOT NULL,
+  [Phone] [nvarchar](11) NOT NULL,
+  [Address] [nvarchar](500) NULL,
+  [City] [int] NULL,
+  [IsLead] [int] NULL CONSTRAINT [DF_Customers_IsLead] DEFAULT (0),
+  CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([ID]),
+  CONSTRAINT [FK_Customers_Customers] FOREIGN KEY ([ID]) REFERENCES [dbo].[Customers] ([ID])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo_Orders.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Orders] (
+  [ID] [int] IDENTITY,
+  [Customer] [int] NOT NULL,
+  [Template] [int] NULL,
+  [Price] [int] NULL,
+  [DateLow] [datetime] NULL,
+  [DateHigh] [datetime] NULL,
+  [Comment] [nvarchar](max) NULL,
+  CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([ID]),
+  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([Customer]) REFERENCES [dbo].[Customers] ([ID])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
